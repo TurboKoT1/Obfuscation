c25fc3d4adfc261fb40d6b297fa620536f9eaafb
TurboKoT <97762147+TurboKoT1@users.noreply.github.com>
2023-04-23 14:44:02 +0300

    Create Obfuscation.py

---
+++ Obfuscation.py
@@ -0,0 +1,36 @@
+import base64, zlib, time, os
+
+while True:
+    print("|| Please enter path to file")
+    path = input("/> ")
+    if path == "" or len(path)<4:
+        print("You're entered incorrect path!")
+    elif not path.endswith(".py"):
+        print("File is should to be py!")
+    elif not os.path.exists(path):
+        print("File not found!")
+    else:
+        start_time = time.time()
+        print("[INFO] * Getting file name..")
+        filename = path.split("/")[-1].split(".")[0]
+
+        print("[INFO] * Getting file code..")
+        code = open(path, 'r', encoding='utf-8').read()
+        print("[INFO] * Encoding file code..")
+        encoded_code = base64.b85encode(code.encode("utf-8"))
+        print("[INFO] * Compressing encoded file code..")
+        compress = zlib.compress(encoded_code)
+
+        unde = f'''exec(base64.b85decode(zlib.decompress({compress})).decode())'''
+        unde_encoded = base64.b85encode(unde.encode("utf-8"))
+        unde_compress = zlib.compress(unde_encoded)
+
+        try:
+            with open(filename + "-obf.py", "x") as obf:
+                obf.write(f'''TurboObfuscate=132;import zlib,base64;exec(base64.b85decode(zlib.decompress({unde_compress})).decode())''')
+                obf.close()
+        except:
+            with open(filename + "-obf.py", "w+") as obf:
+                obf.write(f'''TurboObfuscate=132;import zlib,base64;exec(base64.b85decode(zlib.decompress({unde_compress})).decode())''')
+                obf.close()
+        print(f"[INFO] * Successfully obfuscated in {time.time() - start_time} ms!")
